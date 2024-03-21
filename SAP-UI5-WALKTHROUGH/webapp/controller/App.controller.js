30ccbd2d9dd3b7fdc21833ffff1c23de56bd6557
gokulkaruna <gokulkaruna1@gmail.com>
2024-03-21 20:44:46 +0530

    added i18n

--- SAP-UI5-WALKTHROUGH/webapp/controller/App.controller.js
+++ SAP-UI5-WALKTHROUGH/webapp/controller/App.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
 
  ], (Controller,
 	MessageToast,
-	JSONModel) => {
+	JSONModel,
+	ResourceModel) => {
     "use strict";
  
     return Controller.extend("ui5.walkthrough.controller.App", {
@@ -23,13 +25,29 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
 
+            //set i18n model on view
+            var  i18nModel = new ResourceModel({
+                bundleName :"ui5.walkthrough.i18n.i18n",
+                supporteLocales : [""],
+                fallbackLocales : [""]
+            });
+            this.getView().setModel(i18nModel,"i18n");
+
         },
 
 
 
        onShowHello() {
+
+            //read msg from i18n Model
+
+            var OBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = OBundle.getText("helloMsg",[sRecipient])
+
+
           // show a native JavaScript alert
-          MessageToast.show("Hello There")
+          MessageToast.show(sMsg)
        },
 
 
